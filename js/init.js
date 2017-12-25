27aeb072b4411c46ac3351781248b24fb259ec8b
Koishore Roy <koishore@users.noreply.github.com>
2017-12-26 02:25:15 +0530

    updated site with favicon and working links
    - added favicon
    - added sports to main page
    - added working links everywhere

--- js/init.js
+++ js/init.js
@@ -1,7 +1,11 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
+    $('.button-collapse').sideNav({
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true, // Choose whether you can drag to open on touch screens,
+      }
+    );
     $('.parallax').parallax();
 
   }); // end of document ready
